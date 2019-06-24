2fccfb580564eccc63eec2690bd2783c6b4fa484
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-06-24 06:35:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function switchOnCharmFromTip(tip){
  switch (tip){
    case "generous": return "Thank you so much."; break;
    case "not as generous": return "Thank you."; break;
-   default: return "Bye";
+   default: return "Bye.";
    
  }
 }
