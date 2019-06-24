eab3f7a6ed60f6f22fbd1a6a0d654e20accc0e83
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-06-24 06:33:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ function ternaryCheckCity(city){
 function switchOnCharmFromTip(tip){
  switch (tip){
    case "generous": return "Thank you so much."; break;
+   case "not as generous": return "Thank you."; break;
+   default: return "Bye";
    
  }
 }
