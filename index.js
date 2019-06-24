cca750a0eb0f9af0a20a9efbc34a87183da14970
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-06-24 06:31:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,40 +1,22 @@
 function scuberGreetingForFeet(Value){
  if(Value <= 400){
    return "This one is on me!";
+ }else if (Value > 2000 && Value < 2500){
+   return "I will gladly take your thirty bucks.";
+ }else if ( Value > 2500){
+   return "No can do.";
  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city == "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+ switch (tip){
+   case "generous": 
+ }
 }
-describe('index.js', function () {
-  describe('scuberGreetingForFeet()', function () {
-    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
-    });
-
-    it('charges 30 dollars for a distance over 2000 feet', function () {
-      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
-    });
-
-    it('does not allow rides over 2500 feet', function () {
-      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
-    });
-  });
-
-  describe('ternaryCheckCity()', function () {
-    it('returns "Ok, sounds good." when the city is NYC', function () {
-      expect(ternaryCheckCity('NYC')).to.equal('Ok, sounds good.');
-    });
-
-    it('should return "No go." if the destination city is not NYC', function () {
-      expect(ternaryCheckCity('Pittsburgh')).to.equal('No go.');
-    });
-  });
 
   describe('switchOnCharmFromTip()', function () {
     it('should return "Thank you so much." if the tip is generous', function () {
